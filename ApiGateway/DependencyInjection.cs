7f464c1052056259fe94a715d05e203eef2c9060
uselessgoddess <dodickgod@gmail.com>
2024-10-04 10:34:27 +0300

    Add ocelot as api gateway

---
+++ ApiGateway/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+
+namespace ApiGateway;
+
+public static class DependencyInjection
+{
+    public static WebApplicationBuilder AddApiGateway(this WebApplicationBuilder builder)
+    {
+        builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+        builder.Services.AddOcelot();
+
+        return builder;
+    }
+
+    public static IServiceCollection AddBearer(this IServiceCollection services, IConfiguration config)
+    {
+        var jwt = config.GetSection("Jwt");
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwt.GetValue<string>("Issuer"),
+                    ValidAudience = jwt.GetValue<string>("Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.GetValue<string>("Key"))),
+                };
+            });
+
+        return services;
+    }
+}
